c993faa0e8b9f959970349087fc49c0d2bc09fb0
Ramesht Shukla <36106177+ramesht007@users.noreply.github.com>
2021-09-28 18:02:06 +0800

    initial loop logic

--- fedrec/communications/comm_manager.py
+++ fedrec/communications/comm_manager.py
@@ -3,59 +3,47 @@ from types import FunctionType
 from communication_interfaces import ZeroMQ
 from fedrec.utilities import registry
 from global_comm_stream import CommunicationStream
+from threading import Thread
 from time import sleep
 import asyncio
 
-MESSAGE_HANDLER_DICT = defaultdict(dict)
-
-
-def tag_reciever(message_type):
-
-    def register_handler(func: FunctionType):
-        if func.__name__ in registry:
-            raise LookupError('{} already present {}'.format(
-                message_type, func.__name__))
-        MESSAGE_HANDLER_DICT[message_type] = func
-        return func 
-
-    return register_handler
-
 
 class CommunicationManager:
+    LOOP = asyncio.new_event_loop()
+    THREAD = Thread(target=, daemon=True)
     def __init__(self, config_dict):
         self.com_manager = registry.construct('communications', config_dict)
         self.com_manager.add_observer(self)
         self.message_handler_dict = dict()
-        self.queue = asyncio.Queue()       
+        self.queue = asyncio.Queue()     
 
-    def register_queue(self, receving_id):
-        dic = CommunicationStream.get_global_hash_map()
-        if receving_id in dic:
-            raise LookupError('{} already present in hash_map'.format(receving_id))
-        else:
-            dic[receving_id] = self.queue
     
+    def submit_task(cls):
+        asyncio.set_event_loop(cls.LOOP)
+        cls.LOOP.create_task(self.message_handler())       
+
     def run(self):
-        self.loop = asyncio.get_current_loop()
-        self.loop.create_task(message_handler())
+        asyncio.set_event_loop(self.LOOP)
+        self.LOOP.create_task(self.message_handler())
+
 
     async def send_message(self, message, block=False):
-        # message includes reciever id and sender id
         self.com_manager.send(message)
         if block:
             return await self.com_manager.recieve()
-        else:
-            return
+
 
     async def recieve(self, request_id):
         loop = asyncio.get_current_loop() 
         future = loop.create_future()
         self.message_handler_dict[request_id] = future
         return await future
-        
+
+
     def finish(self):
         self.loop.stop()
         self.com_manager.close()
+
 
     async def message_handler(self):
         while True:
@@ -65,6 +53,7 @@ class CommunicationManager:
                 future.set_result(message)
             else:
                 raise LookupError('{} not in the message dictionary'.format(message.get_request_id()))  
+
 
     def add_to_message_queue(self, message):
         self.queue.put(message)
