cc840ea40e8b56ffd3e2d66d3b3a29ed8b81e732
Varun <varunkhare1234@gmail.com>
2021-11-13 07:46:17 +0000

    remove unnecessary imports

--- fedrec/multiprocessing/mpi_process.py
+++ fedrec/multiprocessing/mpi_process.py
@@ -1,9 +1,9 @@
 from typing import Dict
+from fedrec.communications.messages import JobResponseMessage, JobSubmitMessage
 
 from fedrec.multiprocessing.jobber import Jobber
 from fedrec.python_executors.base_actor import BaseActor
 from fedrec.utilities import registry
-from mpi4py import MPI
 
 
 @registry.load("multiprocessing", "MPI")
@@ -13,23 +13,20 @@ class MPIProcess:
                  worker: BaseActor,
                  logger,
                  com_manager_config: Dict) -> None:
-        self.pool = MPI.COMM_WORLD
-        self.rank = self.pool.Get_rank()
-        self.num_processes = self.pool.Get_size()
         self.jobber = Jobber(worker=worker, logger=logger)
         self.process_comm_manager = registry.construct(
             "communications", config_dict=com_manager_config)
 
     def run(self) -> None:
         while True:
-            job_request = self.process_comm_manager.receive_message()
-            if job_request.JOB_TYPE == "STOP":
+            job_request: JobSubmitMessage = self.process_comm_manager.receive_message()
+            if job_request.job_type == "STOP":
                 return
 
             result = self.jobber.run(job_request)
             self.publish(result)
 
-    def publish(self, job_result) -> None:
+    def publish(self, job_result: JobResponseMessage) -> None:
         self.process_comm_manager.send_message(job_result.result())
 
     def stop(self) -> None:
