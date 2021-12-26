52e938ef090ea4c1d6dbb4b76a473afb104a04c9
Divyanshu <kumardivyanshu4485@gmail.com>
2021-12-26 20:44:39 +0530

    added: Inline Documentation for Kafka Interface

--- fedrec/communications/kafka_interfaces.py
+++ fedrec/communications/kafka_interfaces.py
@@ -7,6 +7,39 @@ from json import loads, dumps
 
 @registry.load("communications", "kafka")
 class Kafka(AbstractCommunicationManager):
+    """
+    Kafka specific methods below here.  These are not part of the abstract class.
+
+    Attributes:
+    ----------
+    serializer: AbstractSerializer
+        The serializer to use.
+    consumer: KafkaConsumer
+        The consumer to use.
+    producer: KafkaProducer
+        The producer to use.
+    consumer_url: str
+        The url of the consumer.
+    consumer_port: int  
+        The port of the consumer.
+    consumer_topic: str
+        The topic of the consumer.
+    producer_url: str
+        The url of the producer.
+    producer_port: int
+        The port of the producer.
+    producer_topic: str 
+        The topic of the producer.
+
+    Methods:
+    --------
+    receive_message():
+        Receives a message from the consumer.
+    send_message(message):
+        Sends a message to the producer.
+    finish():
+        Closes the consumer and producer.
+    """
     def __init__(self,
                  serializer="json",
                  consumer=True,
@@ -39,15 +72,34 @@ class Kafka(AbstractCommunicationManager):
                 group_id=consumer_group_id)
 
     def receive_message(self):
+        """
+        Receives a message from the consumer.
+        
+        Returns:
+        --------
+        message: object
+            The message received.
+        """
         if not self.consumer:
             raise Exception("No consumer defined")
         return next(self.consumer).value
 
     def send_message(self, message):
+        """
+        Sends a message to the producer.
+
+        Returns:
+        --------
+        message: object
+            The message sent.
+        """
         if not self.producer:
             raise Exception("No producer defined")
         self.producer.send(self.producer_topic, value=message)
 
     def finish(self):
+        """
+        Closes the consumer and producer.
+        """
         self.producer.close()
         self.consumer.close()
