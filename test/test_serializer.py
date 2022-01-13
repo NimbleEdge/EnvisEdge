f5cc3c007dd61f35fb37b71632f2071d786c4e4c
ramesht007 <shre611shukla@gmail.com>
2022-01-13 16:45:26 +0000

    removed test class

--- test/test_serializer.py
+++ test/test_serializer.py
@@ -8,32 +8,22 @@ response_message_train = JobResponseMessage("train", "01", "01")
 submit_message_test = JobSubmitMessage("test", "00", "00", "00", "00", "00")
 submit_message_train = JobSubmitMessage("train", "01", "01", "01", "01", "01")
 
-# test class for generate message dict method
-
-
-class test_dict:
-    __type__ = "test"
-
-    def __init__(self, name, id, number) -> None:
-        self.name = name
-        self.id = id
-        self.number = number
-
-# test generate_message_dict method
-
 
 @pytest.mark.parametrize(
     "obj",
-    [test_dict("test", "00", "00"),
-     test_dict("train", "01", "01")]
+    [response_message_test,
+     response_message_train,
+     submit_message_test,
+     submit_message_train]
 )
 def test_generate_message_dict(obj):
+    """test generate_message_dict method
+    """
     dict = AbstractSerializer.generate_message_dict(obj)
     assert dict['__type__'] == obj.__type__
     assert dict['__data__'] == obj.__dict__
 
 
-# test JSOMSerializer method
 @pytest.mark.parametrize(
     "obj, type, data",
     [(response_message_test,
@@ -46,12 +36,12 @@ def test_generate_message_dict(obj):
       submit_message_train.__type__, submit_message_train.__dict__)]
 )
 def test_json_serialize(obj, type, data):
+    """test JSOMSerializer method
+    """
     ans = JSONSerializer.serialize(obj)
     ans = json.loads(ans)
     assert ans['__type__'] == type
     assert ans['__data__'] == data
-
-# test JSOMdeserialize method
 
 
 @pytest.mark.parametrize(
@@ -66,6 +56,8 @@ def test_json_serialize(obj, type, data):
       submit_message_train.__type__, submit_message_train.__dict__)]
 )
 def test_json_deserialize(message_dict, type, data):
+    """test JSOMdeserialize method
+    """
     obj = JSONSerializer.serialize(message_dict)
     ans = JSONSerializer.deserialize(obj)
     assert ans.__type__ == type
