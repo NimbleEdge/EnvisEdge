1f2976e134743920c88b47aeec7153012fb984f2
Yash Vardhan Sharma <56366274+Yash-Vardhan-Sharma@users.noreply.github.com>
2022-01-29 11:25:00 +0530

    added more tests and fixed checking

--- test/test_messages.py
+++ test/test_messages.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 import yaml
-from fedrec.communications.messages import JobSubmitMessage, JobResponseMessage
+from fedrec.communications.messages import Message, JobSubmitMessage, JobResponseMessage
 
 from fedrec.python_executors.base_actor import ActorState
 
@@ -16,6 +16,21 @@ def pytest_generate_tests(metafunc):
         params = config[fct_name]
         metafunc.parametrize(params["params"], params["values"])
 
+def test_message(senderid, receiverid):
+    
+    obj = Message(senderid, receiverid)
+    
+    assert obj.senderid == senderid
+    assert obj.receiverid == receiverid
+    assert obj.__type__ == obj.__class__.__name__
+
+def test_worker_state(job_type, job_args , job_kwargs,
+                  senderid, receiverid, workerstate):
+
+    obj = JobSubmitMessage(job_type, job_args, job_kwargs,
+                           senderid, receiverid, workerstate)
+    
+    assert obj.worker_state() == workerstate
 
 def test_job_type(job_type, job_args , job_kwargs,
                   senderid, receiverid, workerstate):
@@ -25,12 +40,15 @@ def test_job_type(job_type, job_args , job_kwargs,
 
     assert type(obj.get_job_type()) == str
     assert obj.get_job_type() == job_type
-
+    
 def test_jobresponse_status(job_type, senderid, receiverid):
     
     obj = JobResponseMessage(job_type, senderid, receiverid)
 
-    assert(type(obj.status) == bool)
+    if obj.errors is None:
+        assert obj.status == True
+    else:
+        assert obj.status == False
 
 def test_jobsubmitmessage(job_type, job_args , job_kwargs,
                           senderid, receiverid, workerstate):
