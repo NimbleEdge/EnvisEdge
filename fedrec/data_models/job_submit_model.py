69b31f25edcc0d24588f2d9c4abd5088cdae287b
Yash Vardhan Sharma <56366274+Yash-Vardhan-Sharma@users.noreply.github.com>
2022-02-07 12:15:29 +0530

    Update job_submit_model.py

--- fedrec/data_models/job_submit_model.py
+++ fedrec/data_models/job_submit_model.py
@@ -4,7 +4,9 @@ from fedrec.python_executors.base_actor import ActorState
 from fedrec.utilities import registry
 from dataclasses import dataclass
 from fedrec.data_models.messages import Message
-from fedrec.serialization.abstract_serializer import AbstractSerializer, get_serializer, serializable_with
+from fedrec.serialization.abstract_serializer import (AbstractSerializer,
+                                                      get_serializer,
+                                                      serializable_with)
 
 class JobSubmitSerializer(AbstractSerializer):
     
@@ -14,18 +16,25 @@ class JobSubmitSerializer(AbstractSerializer):
     def serialize(self, obj):
         response_dict = {}
         response_dict["job_type"] = obj.job_type
-        response_dict["job_args"] = [self.serialize_attribute(arg) for arg in obj.job_args]
-        response_dict["job_kwargs"] = [self.serialize_attribute(kwarg) for kwarg in obj.job_kwargs]
+        response_dict["job_args"] = [self.serialize_attribute(arg)
+                                     for arg in obj.job_args]
+        response_dict["job_kwargs"] = [self.serialize_attribute(kwarg)
+                                       for kwarg in obj.job_kwargs]
         response_dict["worker_state"] = self.serialize_attribute(obj.workerstate)
 
         return self.serialization_strategy.unparse(response_dict)
 
     def deserialize(self, obj : Dict):
-        job_args = [self.deserialize_attribute(arg) for arg in obj.job_args]
-        job_kwargs = [self.deserialize_attribute(kwarg) for kwarg in obj.job_kwargs]
+        job_args = [self.deserialize_attribute(arg)
+                    for arg in obj.job_args]
+        job_kwargs = [self.deserialize_attribute(kwarg)
+                      for kwarg in obj.job_kwargs]
         worker_state = self.deserialize_attribute(obj.workerstate)
 
-        JobSubmitMessage(obj["job_type"], job_args, job_kwargs, worker_state)
+        JobSubmitMessage(obj["job_type"],
+                         job_args,
+                         job_kwargs,
+                         worker_state)
         return super().deserialize()
 
 
