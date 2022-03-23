b8c54d61f601935c49b648c266604f381a8d5e04
Divyanshu <kumardivyanshu4485@gmail.com>
2022-03-23 15:23:27 +0530

    fixed: pep8 errors

--- experiments/FedRecon/utilities.py
+++ experiments/FedRecon/utilities.py
@@ -2,20 +2,25 @@ from typing import Callable, Optional, Tuple
 
 import torch
 
-DatasetSplitFn = Callable[[torch.Dataset, torch.tenosr], Tuple[torch.Datasest,
-                                                                torch.Dataset]]
+DatasetSplitFn = Callable[[torch.Dataset, torch.tenosr],
+                          Tuple[torch.Datasest, torch.Dataset]]
+
+
 def build_dataset_split(recon_epochs_max: int = 1,
                         recon_epochs_constant: bool = True,
-                           recon_steps_max: Optional[int] = None,
-                           post_recon_epochs: int = 1,
-                           post_recon_steps_max: Optional[int] = None,
-                           split_dataset: bool = False) -> DatasetSplitFn:
+                        recon_steps_max: Optional[int] = None,
+                        post_recon_epochs: int = 1,
+                        post_recon_steps_max: Optional[int] = None,
+                        split_dataset: bool = False) -> DatasetSplitFn:
 
-    recon_condition = lambda i, entry: torch.tensor(i)%torch.tensor(2)==0
-    post_recon_condition = lambda i, entry: torch.tensor(i)%torch.tensor(2)> 0
-    get_entry = lambda i, entry: entry
-    
-    def dataset_split(clinet_dataset,round_num: torch.Tensor):
+    def recon_condition(i, entry): return torch.tensor(
+        i) % torch.tensor(2) == 0
+    def post_recon_condition(i, entry): return torch.tensor(
+        i) % torch.tensor(2) > 0
+
+    def get_entry(i, entry): return entry
+
+    def dataset_split(clinet_dataset, round_num: torch.Tensor):
         if split_dataset:
             recon_dataset = clinet_dataset.enumerate().filter(
                 recon_condition).map(get_entry)
@@ -43,6 +48,6 @@ def build_dataset_split(recon_epochs_max: int = 1,
         post_recon_dataset = post_recon_dataset.repeat(post_recon_epochs)
         if post_recon_steps_max is not None:
             post_recon_dataset = post_recon_dataset.take(post_recon_steps_max)
-        
+
         return recon_dataset, post_recon_dataset
     return dataset_split
