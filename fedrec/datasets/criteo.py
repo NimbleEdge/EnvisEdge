d16135d58e57feccccdf61eee9ea179f0ffd7d61
Somasree Majumder <56045049+soma2000-lang@users.noreply.github.com>
2022-05-01 18:13:49 +0530

    Update criteo.py

--- fedrec/datasets/criteo.py
+++ fedrec/datasets/criteo.py
@@ -6,32 +6,27 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset 
 class CriteoDataset(Dataset):
-  
+ """
+    It's a simple dataloader for criteo
 
+    ...
 
+    Attributes
+    ----------
+    Dataset  : The dataset to be loaded
+    X_int:An array in the integer form
+    X_cat:its an array storing some tensor value
+    y:its an array storing some values
+    max_ind_range: for defining the maximum range of indices
 
-
-
-""""This class is used to represent the criteo dataset 
-
-
-...
-Attributes
------------
-X_int:
-X_cat:its an array storing some tensor value
-y:its an array storing some values
-max_ind_range: for defining the maximum range of indices
-    
-Methods 
-----------------------------------------------------------------
-__getitem__
-_default_preprocess
-collate_wrapper_criteo_offset
-offset_to_length_converter"""
-
-
-
+    Methods
+    -------
+    __getitem__(c='rgb')
+    _default_preprocess   Used to preprocess the data in the dataset
+    collate_wrapper_criteo_offset
+    offset_to_length_converter  Changes from offset to length 
+    make_criteo_data_and_loader 
+    """
 
     def __init__(                       
             self,                   
@@ -42,7 +37,10 @@ offset_to_length_converter"""
         self.X_cat = X_cat
         self.y = y
 
-    def __getitem__(self, index): #_getitem_() allows its instances to use the [] (indexer) operators
+    def __getitem__(self, index): 
+        '''_getitem_() allows its instances to use the [] (indexer) operators
+        Input
+        index:int -takes the index as input'''
 
         if isinstance(index, slice):
             return [
@@ -61,6 +59,13 @@ offset_to_length_converter"""
             return self.X_int[index], self.X_cat[index], self.y[index]
 
     def _default_preprocess(self, X_int, X_cat, y):
+        """It does the default preprocesses
+        Input
+        X_int:tensor  Integer variable
+        X_cat:tensor Categorical variable
+        y:tensor 
+        
+        Returns:tensor the integer variable and the categorical variable"""
         X_int = torch.log(torch.tensor(X_int, dtype=torch.float) + 1)
         if self.max_ind_range > 0:
             X_cat = torch.tensor(X_cat % self.max_ind_range, dtype=torch.long)
@@ -71,11 +76,18 @@ offset_to_length_converter"""
         return X_int, X_cat, y #returns the integer variable and the categorical variable
 
     def __len__(self):
-        return len(self.y) #returns the length
+        """Returns the length"""
+      
+        return len(self.y) 
 
 
 def collate_wrapper_criteo_offset(list_of_tuples):
-    # where each tuple is (X_int, X_cat, y)
+    """This function collects and combines the bias_value(offset) which will be used during model training
+    Input
+    ----------
+    list_of_tuples  : int returns tuples where each tuple is (X_int, X_cat, y)"""
+   
+
     transposed_data = list(zip(*list_of_tuples))#data represented in form of a list
     X_int = torch.log(torch.tensor(transposed_data[0], dtype=torch.float) + 1)#returns a new tensor with the natural logarithm of the elements of input
     X_cat = torch.tensor(transposed_data[1], dtype=torch.long)
@@ -93,20 +105,35 @@ def collate_wrapper_criteo_offset(list_of_tuples):
 
 
 def offset_to_length_converter(lS_o, lS_i):
+    """This function converts the bias value to length converter
+    Input 
+    lS_o:int 
+    lS_i: int
+    """
     def diff(tensor):
         return tensor[1:] - tensor[:-1] #returns the length of the tensor
 
     return torch.stack(   #Concatenates a sequence of tensors along a new dimension.
 
         [
-            diff(torch.cat((S_o, torch.tensor(lS_i[ind].shape))).int())#Concatenates the given sequence of seq tensors in the given dimension
-            for ind, S_o in enumerate(lS_o)                             and computes the n-th forward difference along the given dimension#
+            diff(torch.cat((S_o, torch.tensor(lS_i[ind].shape))).int())
+
+            """Concatenates the given sequence of seq tensors in the given dimension and computes the n-th forward difference along the given dimension""" 
+            for ind, S_o in enumerate(lS_o)  
         ]
     )
 
 
 def collate_wrapper_criteo_length(list_of_tuples):
     # where each tuple is (X_int, X_cat, y)
+"""This function collects and combines the length of the criteo
+    
+    Parameters
+    ----------
+    list_of_tuples  : int """
+
+
+
     transposed_data = list(zip(*list_of_tuples))#Joins two tuples together
     X_int = torch.log(torch.tensor(transposed_data[0], dtype=torch.float) + 1)
     X_cat = torch.tensor(transposed_data[1], dtype=torch.long)#It creates a multi-dimensional matrix containing elements of a single data type.
@@ -125,6 +152,13 @@ def collate_wrapper_criteo_length(list_of_tuples):
 
 
 def make_criteo_data_and_loaders(args, offset_to_length_converter=False):
+    '''
+    Input
+    args:takes the arguments
+    offset_to_length_converter:bool initial value has been taken false
+
+
+     Returns: the train data,train loader and test loader'''
     train_data = CriteoDataset(
         args.data_set,
         args.max_ind_range,
@@ -155,7 +189,7 @@ def make_criteo_data_and_loaders(args, offset_to_length_converter=False):
         shuffle=False,
         num_workers=args.num_workers,
         collate_fn=collate_wrapper_criteo,
-        pin_memory=False,
+        pin_memory=False,# custom memory pinning is false
         drop_last=False,  # True
     )
 
@@ -166,7 +200,7 @@ def make_criteo_data_and_loaders(args, offset_to_length_converter=False):
         num_workers=args.test_num_workers,
         collate_fn=collate_wrapper_criteo,
         pin_memory=False,
-        drop_last=False,  # True
+        drop_last=False,  
     )
 
-    return train_data, train_loader, test_data, test_loader #returning the train data,train loader and test loader
+    return train_data, train_loader, test_data, test_loader
