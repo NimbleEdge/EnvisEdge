667a0413909a6af6365e7e0575cf8de482ce5b9a
Haripriya <arrahppk@gmail.com>
2022-06-29 21:26:41 +0530

    added example for zeromq

--- fedrec/communication_interfaces/zeroMQ_interface.py
+++ fedrec/communication_interfaces/zeroMQ_interface.py
@@ -73,6 +73,26 @@ class ZeroMQ(AbstractCommunicationManager):
         --------
         message: object
             The message received.
+
+        Example
+        -------
+        >>> import zmq
+        >>> context = zmq.Context()
+
+        #  Socket to talk to server
+        >>> print("Connecting to envisedge server…")
+        >>> socket = context.socket(zmq.REQ)
+        >>> socket.connect("tcp://localhost:5555")
+
+        #  Do 10 requests, waiting each time for a response
+        >>> for request in range(10):
+        >>>     print("Sending request %s …" % request)
+        >>>     socket.send(b"Hello")
+
+                #  Get the reply.
+        >>>     message = socket.recv()
+        >>>     print("Received reply %s [ %s ]" % (request, message))
+
         """
 
         if not self.subscriber:
@@ -87,6 +107,27 @@ class ZeroMQ(AbstractCommunicationManager):
         -------
         message: object
             The message sent.
+
+        Example
+        -------
+        >>> import time
+        >>> import zmq
+
+        >>> context = zmq.Context()
+        >>> socket = context.socket(zmq.REP)
+        >>> socket.bind("tcp://*:5555")
+
+        >>> while True:
+              #  Wait for next request from client
+        >>>      message = socket.recv()
+        >>>      print("Received request: %s" % message)
+
+              #  Do some 'work'
+        >>>      time.sleep(1)
+
+              #  Send reply back to client
+        >>>      socket.send(b"World")
+
         """
 
         if not self.publisher:
